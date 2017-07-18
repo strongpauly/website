b9aded6d0cee3d7956c95a47eca2d5e7ef5c145e
Paul Potsides <strongpauly@hotmail.com>
2017-07-18 08:58:24 +0100

    Whats on potsides.com

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Tab, Nav, PageHeader, Grid, Row, Col, NavItem} from 'react-bootstrap';
+import {Tab, Nav, Grid, Row, Col, NavItem} from 'react-bootstrap';
 
 class App extends Component {
   constructor() {
@@ -20,19 +20,22 @@ class App extends Component {
     return (
       <Grid>
         <Row>
+          <Col>&nbsp;
+          </Col>
+        </Row>
+        <Row>
           <Col>
-            <PageHeader>HelloWorld!</PageHeader>
-            <div>
-              Collective works and pet projects of Paul Potsides:
-            </div>
-            <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+            <Tab.Container id="left-tabs-example" defaultActiveKey="desc">
               <Row className="clearfix">
                 <Col sm={3}>
                   <Nav bsStyle="pills" stacked>
-                    <NavItem eventKey="first">
+                    <NavItem eventKey="desc">
+                     Description
+                    </NavItem>
+                    <NavItem eventKey="minesweeper">
                      Minesweeper
                     </NavItem>
-                    <NavItem eventKey="second">
+                    <NavItem eventKey="wordgame">
                       WordGame
                     </NavItem>
                     <NavItem eventKey="cv">
@@ -42,14 +45,21 @@ class App extends Component {
                 </Col>
                 <Col sm={9}>
                   <Tab.Content animation>
-                    <Tab.Pane eventKey="first">
-                      <iframe src="/minesweeper" title="minesweeper" style={{border:0, width:'100%', height:'100%'}}></iframe>
+                    <Tab.Pane eventKey="desc">
+                      This is a portfolio of projects used in investigations into technologies by Paul Potsides.
                     </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                      <iframe src="/wordgame" title="wordgame" style={{border:0, width:'100%', height:'100%'}}></iframe>
+                    <Tab.Pane eventKey="minesweeper">
+                      Minesweeper implemented using react.
+                      <iframe src="/minesweeper" title="minesweeper"></iframe>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="wordgame">
+                      A simple word game implemented using react.  Select words of the length suggested by the box below the grid.  The + will start a new game.
+                      The ♺ will reset the game. The ? will provide a hint.  If the game can no longer be completed, the ♺ will rotate.
+                      <iframe src="/wordgame" title="wordgame"></iframe>
                     </Tab.Pane>
                     <Tab.Pane eventKey="cv">
-                      <iframe src="/cv" title="cv" style={{border:0, width:'100%', height:'100%'}}></iframe>
+                      CV implemented using react and react-bootstrap.
+                      <iframe src="/cv" title="cv"></iframe>
                     </Tab.Pane>
                   </Tab.Content>
                 </Col>
