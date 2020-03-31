a258db1ef940a78c5a3548dd1028ebd8ae4c7127
Paul Potsides <paul@zero-1.co>
2020-03-31 13:52:56 +0200

    Add an infrastructure target and paul.potsides.com A record.

--- infrastructure/app-deployment/main.tf
+++ infrastructure/app-deployment/main.tf
@@ -17,9 +17,21 @@ data "aws_route53_zone" "zone" {
   name = var.domain
 }
 
-resource "aws_route53_record" "A" {
+resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.zone.zone_id
   name    = local.appUrl
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "paul" {
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "paul.potsides.com"
   type    = "A"
 
   alias {
