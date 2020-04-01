224e6ac0f3684f1c8318f9fd09e86cc5586148fe
Paul Potsides <paul@zero-1.co>
2020-04-01 19:06:23 +0200

    No node or java projects in the pipeline.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Home extends Component {
-
   render() {
-    return <div id="homeContainer"><p>This is a portfolio of projects used in investigations into technologies by London based, full stack developer, Paul Potsides.</p>
-      <p>Currently, this is mainly client side and react based projects, but I will hopefully be expanding this into some Java or Node based middle tier projects too.</p>
-      <p>This site was built using react, react-bootstrap and react-router.  It&apos;s source can be found <a href="https://www.github.com/strongpauly/website">here</a>
-      </p>
-    </div>;
+    return (
+      <div id="homeContainer">
+        <p>
+          This is a portfolio of projects used in investigations into
+          technologies by London based, full stack developer, Paul Potsides.
+        </p>
+        <p>Currently, this is mainly client side and react based projects.</p>
+        <p>
+          This site was built using react, react-bootstrap and react-router.
+          It&apos;s source can be found{" "}
+          <a href="https://www.github.com/strongpauly/website">here</a>
+        </p>
+      </div>
+    );
   }
-
 }
