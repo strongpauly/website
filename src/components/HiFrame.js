29680974b4a0f8320e8d2144148832fb7b9b3b8c
Paul Potsides <paul@zero-1.co>
2020-04-21 14:56:23 +0200

    Be more protective over HiFrame resizing.

--- src/components/HiFrame.js
+++ src/components/HiFrame.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export class HiFrame extends Component {
   static propTypes = {
     src: PropTypes.string,
-    title: PropTypes.string
+    title: PropTypes.string,
+  };
+
+  constructor() {
+    super();
+    this.ref = React.createRef();
   }
+
+  //Resizes the iframe to it's content, then sets focus.
+  fixFrame = (e) => {
+    const iframe = e.target;
+    if (iframe.contentDocument) {
+      iframe.style.height = iframe.contentDocument.body.scrollHeight + "px";
+      iframe.contentWindow.focus();
+    } else if (this.ref && this.ref.current) {
+      setTimeout(() => {
+        this.fixFrame({ target: this.ref.current });
+      }, 1000);
+    }
+  };
 
   render() {
-    //Resizes the iframe to it's content, then sets focus.
-    function fixFrame(e) {
-      const iframe = e.target;
-      iframe.style.height = iframe.contentDocument.body.scrollHeight + 'px';
-      iframe.contentWindow.focus();
-    }
-    return <iframe src={this.props.src} title={this.props.title} onLoad={fixFrame}></iframe>;
+    return (
+      <iframe
+        ref={this.ref}
+        src={this.props.src}
+        title={this.props.title}
+        onLoad={(e) => this.fixFrame(e)}
+      ></iframe>
+    );
   }
-
 }
